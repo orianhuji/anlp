9b6068c5b17ca04a4d50059a0122823f11ecd0b2
Orian <oriangeneral@gmail.com>
2024-06-03 05:36:03 +0300

    WIP - other tranlations & logics

--- translate_russian_to_english.py
+++ translate_russian_to_english.py
@@ -7,9 +7,19 @@ import os.path
 
 from tqdm import tqdm
 import torch
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
-pipe = pipeline("text-generation", model="Unbabel/TowerInstruct-v0.1", torch_dtype=torch.bfloat16, device_map="auto")
+# TRANSLATION_MODEL = 'Unbabel/TowerInstruct-v0.1'
+#
+# pipe = pipeline("text-generation", model=TRANSLATION_MODEL, torch_dtype=torch.bfloat16, device_map="auto")
+
+TRANSLATION_MODEL = 'facebook/nllb-200-distilled-600M'
+
+model = AutoModelForSeq2SeqLM.from_pretrained(TRANSLATION_MODEL)
+tokenizer = AutoTokenizer.from_pretrained(TRANSLATION_MODEL)
+
+pipe = pipeline('translation', model=model, tokenizer=tokenizer, src_lang='rus_Cyrl', tgt_lang='eng_Latn',
+                max_length=512)
 
 # We use the tokenizer’s chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
 
@@ -24,48 +34,59 @@ pipe = pipeline("text-generation", model="Unbabel/TowerInstruct-v0.1", torch_dty
 if os.path.exists('./data/translation_cache.json'):
     with open('./data/translation_cache.json', 'r', encoding='utf-8') as translation_f:
         TRANSLATION_CACHE = json.loads(translation_f.read() or '{}')
+        TRANSLATION_CACHE[TRANSLATION_MODEL] = TRANSLATION_CACHE.get(TRANSLATION_MODEL, {})
 else:
-    TRANSLATION_CACHE = {}
+    TRANSLATION_CACHE = {TRANSLATION_MODEL: {}}
+
 SPLIT_BY = '\nEnglish:<|im_end|>\n<|im_start|>assistant\n'
+
+
 def fix_translation(in_text):
-    return in_text[in_text.find(SPLIT_BY)+len(SPLIT_BY):]
+    return in_text[in_text.find(SPLIT_BY) + len(SPLIT_BY):]
+
 
 def translate(input_txt):
-    if TRANSLATION_CACHE.get(input_txt):
-        return TRANSLATION_CACHE[input_txt]
+    if TRANSLATION_CACHE[TRANSLATION_MODEL].get(input_txt):
+        return TRANSLATION_CACHE[TRANSLATION_MODEL][input_txt]
+    if 'nllb' in TRANSLATION_MODEL:
+        outputs = _nllb_translate(input_txt)
+    else:
+        outputs = _tower_translate(input_txt)
+        outputs = fix_translation(outputs[0]["generated_text"])
+
+    TRANSLATION_CACHE[TRANSLATION_MODEL][input_txt] = outputs
+    with open('./data/translation_cache.json', 'w', encoding='utf-8') as translation_f:
+        translation_f.write(json.dumps(TRANSLATION_CACHE, indent=4, ensure_ascii=False))
+    return TRANSLATION_CACHE[TRANSLATION_MODEL][input_txt]
+
+
+def _nllb_translate(input_txt):
+    output = pipe(input_txt)
+    return output[0]['translation_text']
+
+
+def _tower_translate(input_txt):
     messages = [{
         "role": "user",
         "content": f"Translate the following text from Russian into English.\nRussian: {input_txt}\nEnglish:"
     }, ]
     prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
     outputs = pipe(prompt, max_new_tokens=512, do_sample=False)
-    TRANSLATION_CACHE[input_txt] = fix_translation(outputs[0]["generated_text"])
-    with open('./data/translation_cache.json', 'w', encoding='utf-8') as translation_f:
-        translation_f.write(json.dumps(TRANSLATION_CACHE, indent=4, ensure_ascii=False))
-    return TRANSLATION_CACHE[input_txt]
+    return outputs
 
 
 with open('./data/original.json', 'r', encoding='utf-8') as original_ds_f:
     original_ds = json.loads(original_ds_f.read())
 
-with open('./data/translated.json', 'r', encoding='utf-8') as translated_ds_f:
-    translated_ds = json.loads(translated_ds_f.read())
-
 invalid_counter = 0
-for idx, record in tqdm(enumerate(original_ds['data'])):
-    # record['title_eng'] = translate(record['title'])
-    record['title_eng'] = translated_ds[idx]['title_eng']
-    TRANSLATION_CACHE[record['title']] = record['title_eng']
+for record in tqdm(original_ds['data']):
+    record['title_eng'] = translate(record['title'])
 
-    for idxx, paragraph in tqdm(enumerate(record['paragraphs'])):
-        # paragraph['context_en'] = translate(paragraph['context'])
-        paragraph['context_en'] = translated_ds[idx]['paragraphs'][idxx]['context_en']
-        TRANSLATION_CACHE[paragraph['context']] = paragraph['context_en']
+    for paragraph in tqdm(record['paragraphs']):
+        paragraph['context_en'] = translate(paragraph['context'])
 
-        for idxxx, qa in enumerate(paragraph['qas']):
-            # qa['question_en'] = translate(qa['question'])
-            qa['question_en'] = translated_ds[idx]['paragraphs'][idxx]['qas'][idxxx]['question_en']
-            TRANSLATION_CACHE[qa['question']] = qa['question_en']
+        for qa in paragraph['qas']:
+            qa['question_en'] = translate(qa['question'])
 
             for answer in qa['answers']:
                 answer['text_en'] = translate(answer['text'])
@@ -76,16 +97,9 @@ for idx, record in tqdm(enumerate(original_ds['data'])):
                         answer['answer_end_en'] = answer['answer_start_en'] + len(answer['text_en'])
                     else:
                         invalid_counter += 1
-with open('./data/translation_cache.json', 'w', encoding='utf-8') as translation_f:
-    translation_f.write(json.dumps(TRANSLATION_CACHE, indent=4, ensure_ascii=False))
+
 print(f'number of invalid translations => {invalid_counter}')
+original_ds['invalid_cnt'] = invalid_counter
 
-with open('./data/translated.json', 'w', encoding='utf-8') as translated_ds_f:
-    translated_ds_f.write(json.dumps(original_ds['data'], indent=4, ensure_ascii=False))
-
-
-
-
-
-
-
+with open(f'./data/translated_{TRANSLATION_MODEL}.json', 'w', encoding='utf-8') as translated_ds_f:
+    translated_ds_f.write(json.dumps(original_ds, indent=4, ensure_ascii=False))
