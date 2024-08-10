c3d646afe0a965d762b82420c85c24c3b1f27327
Ravid Levy <ravid.levy@mail.huji.ac.il>
2024-08-11 01:13:33 +0300

    add plots

--- src/tensorboard_parser.py
+++ src/tensorboard_parser.py
@@ -1,1 +1,18 @@
-#TODO
+from tensorboard.backend.event_processing import event_accumulator
+import matplotlib.pyplot as plt
+
+# TODO: For every file in original proj
+event_acc = event_accumulator.EventAccumulator("..\original_proj\logs\en_bert\events.out.tfevents.1721666458.orian-System-Product-Name.199637.9")
+event_acc.Reload()
+
+for scalar in ['train/loss', 'eval/loss']:
+    print(scalar)
+    x = []
+    y = []
+    for event in event_acc.Scalars(scalar):
+        print(f"step: {event.step}, value: {event.value}")
+        x.append(event.step)
+        y.append(event.value)
+
+    plt.plot(x, y)
+    plt.show()
